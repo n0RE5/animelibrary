9e5c65e07100414a0e3c3b5267ad3d4604dd9bd8
n0RE <nore@desktop>
2023-02-07 19:11:04 +0300

    fix: fixed useFetching hook

--- client/src/hooks/useFetching.ts
+++ client/src/hooks/useFetching.ts
@@ -4,10 +4,10 @@ export const useFetching = (callback: any) => {
     const [isLoading, setIsLoading] = useState<boolean>(false)
     const [error, setError] = useState<string>('')
 
-    const fetching = async () => {
+    const fetching = async (...args) => {
         try {
             setIsLoading(true)
-            await callback()
+            await callback(...args)
         } catch(e: any) {
             setError(e.message)
         } finally {
