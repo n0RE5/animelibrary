3c7ee88aa00a3c9c436b112ab2e7fca8d1b41d5a
n0RE <nore@desktop>
2023-02-08 18:05:28 +0300

    upd: added sort function of user watchlist

--- client/src/components/UserProfileComponents/WatchList.tsx
+++ client/src/components/UserProfileComponents/WatchList.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import classes from '../../pages/styles/UserProfilePage.module.scss'
 import { AnimeItemI } from '../../types/Global';
+import AnimeSort from '../AnimeSort';
 import WatchListPoster from '../UI/WatchListPoster/WatchListPoster';
 
 
 const WatchList: React.FC = () => {
     //<AnimeSort animeList={animeList} setSortedList={setAnimeList} />
-    const userWatchList: AnimeItemI[] = useSelector((state: any) => state.userState.watchList); 
+    const dispatch = useDispatch();
+    const userWatchList: AnimeItemI[] = useSelector((state: any) => state.userState.watchList);
+
+    const sortUserWatchlist = (sortedList: AnimeItemI) => {
+        dispatch({type: "SET_WATCHLIST", payload: sortedList})
+    }
+
     return (
         <div className={classes.UPP_body}>
             <div className={classes.media_info}>
                 <div className={classes.media_info_title}>Список аниме</div>
                 <div className={classes.media_info_description}>Вы отметили эти аниме как посмотреть позже</div>
+                <hr />
+                    <AnimeSort animeList={userWatchList} setSortedList={sortUserWatchlist} />
                 <hr />
             </div>
             <div className={classes.anime_container}>
