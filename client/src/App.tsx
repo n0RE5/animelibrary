0a300f163cd309a79bc60dbe6af43e53beb2c9f8
n0RE <nore@desktop>
2023-02-16 19:04:18 +0300

    upd: optimized fetching logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,33 +3,36 @@ import AppRouter from './components/AppRouter';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import './styles/App.scss'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 import { check } from './http/userAPI';
 import { getAnimeList } from './http/animeAPI';
 import { useFetching } from './hooks/useFetching';
 import { setAuth, setUser } from './store/userSlice';
 import { setGlobalList } from './store/globalListSlice';
+import { AnimeItemI } from './types/Global';
 
 function App() {
-  // todo loading screen
   const dispatch = useDispatch()
   const [loading, setIsLoading] = useState(true);
+  const globalList: AnimeItemI[] = useSelector((state: any) => state.globalList.animeList)
 
   const [fetchList] = useFetching( async () => {
     const animeList = await getAnimeList(10, 1)
     return dispatch(setGlobalList( animeList.data.rows))
   })
 
-  useEffect(() => { 
-    fetchList()
-  }, [])
-
   useEffect(() => {
+
+    if (!globalList.length) {
+      fetchList()
+    }
+
     check().then((data: any) => {
       dispatch(setAuth(true))
       dispatch(setUser(data))
     }).finally(() => setIsLoading(false))
+    
   }, []);
 
   return (
